f060adb644b1c4eb3aeb32e2140718991bf265ce
globefire <sahun5774@gmail.com>
2018-12-13 16:39:36 +0530

    js added

---
+++ land.js
@@ -0,0 +1,10 @@
+function MyFunc(){
+
+	if(screen.width > 1024){
+	window.location.assign("./home.php");
+	}
+	else{
+		window.location.assign("./home.php"); //mobile view running on localhost
+	}
+}
+
