3fd198715da982840fae590ca6f326aeff6b1593
globefire <sahun5774@gmail.com>
2019-05-30 21:13:42 +0530

    mailer was missing

--- home.php
+++ home.php
@@ -3,9 +3,10 @@
 	use PHPMailer\PHPMailer\PHPMailer;
 	use PHPMailer\PHPMailer\Exception;
 
-	require '../PHPMailer/src/Exception.php';
-	require '../PHPMailer/src/PHPMailer.php';
-	require '../PHPMailer/src/SMTP.php';
+	require './PHPMailer/src/Exception.php';
+	require './PHPMailer/src/PHPMailer.php';
+	require './PHPMailer/src/SMTP.php';
+
 
 	if (isset($_POST['submit'])) {
 		# code...
